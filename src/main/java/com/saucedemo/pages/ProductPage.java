7b2d911efb448b7efde3d4c63f4cc98d5d1941e0
Vishal <vikytyagi1800@gmail.com>
2024-10-06 15:31:14 +0530

    Third Commit

--- src/main/java/com/saucedemo/pages/ProductPage.java
+++ src/main/java/com/saucedemo/pages/ProductPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class ProductPage extends BasePage{
 
-    private By productHeader=By.xpath("/span[Text()='Products']");
+    private final By productHeader=By.xpath("//*[@id=\"header_container\"]//span");
 
     public boolean isProductHeaderDisplayed(){
         return find(productHeader).isDisplayed();
